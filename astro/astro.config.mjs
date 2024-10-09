2f092e75480c6dff8d7d0d974a675bac415cb20a
BayernMuller <cjjunk29@gmail.com>
2024-10-09 19:48:28 +0900

    feat: update Astro dependencies, add Korean documentation, and configure build process

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,9 +5,15 @@ import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import remarkMath from 'remark-math'
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   integrations: [mdx(), preact(), react(), sitemap()],
-  base: `/`,
-  site: `https://advanced-astro.dev`
+  base: `/deep-learning-specialization/`,  // GitHub Pages 프로젝트 경로로 수정
+  site: `https://theintrance.github.io/deep-learning-specialization`,  // GitHub Pages 배포 URL로 수정
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
 })
---
+++ build.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+PUBLIC_PATH="astro/public"
+KO_DOCS_PATH="astro/src/content/docs/ko"
+CONSTS_PATH="astro/src/consts.ts"
+
+rm -rf $KO_DOCS_PATH
+mkdir -p $KO_DOCS_PATH
+
+# copy README.md to introduction.md
+cp README.md $KO_DOCS_PATH/introduction.md
+
+# copy assets to public
+cp -r assets $PUBLIC_PATH
+
+# remove current sidebar consts
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' '/export const SIDEBAR: Sidebar = {/,/^}/d' $CONSTS_PATH
+else
+    sed -i '/export const SIDEBAR: Sidebar = {/,/^}/d' $CONSTS_PATH
+fi
+
+# copy chapters to ko/chapter
+chapters=$(find . -maxdepth 1 -type d -name '[0-9]_*' | sort -V)
+for chapter in $chapters; do
+    mkdir -p "$KO_DOCS_PATH/$chapter"
+    cp -r "$chapter" "$KO_DOCS_PATH"
+done
+
+# add sidebar consts
+echo "export const SIDEBAR: Sidebar = {" | tee -a $CONSTS_PATH
+echo "  ko: {" | tee -a $CONSTS_PATH
+for chapter in $chapters; do
+    header=$(basename "${chapter/[0-9]_/}")
+    header=${header//_/ }
+    echo "    '$header': {" | tee -a $CONSTS_PATH
+    
+    sub_chapters=$(find "$chapter" -maxdepth 1 -type d -name '[0-9][0-9]_*' | sort -V)
+    for sub_chapter in $sub_chapters; do
+        sub_header=$(basename "${sub_chapter/[0-9][0-9]_/}")
+        sub_header=${sub_header//_/ }
+        echo "      '$sub_header': [" | tee -a $CONSTS_PATH
+        
+        contents=$(find "$sub_chapter" -maxdepth 1 -type f -name '*.mdx' | sort -V)
+        for content in $contents; do
+            content=${content/.mdx/}
+            content=${content#./}
+            content_name=$(basename "${content/ko\//}")
+            
+            content_name=${content_name//[0-9][0-9]_/}
+            content_name=${content_name//_/ }
+
+            echo "        { text: '$content_name', link: '/ko/$content' }," | tee -a $CONSTS_PATH
+        done
+        echo "      ]," | tee -a $CONSTS_PATH
+    done
+    echo "    }," | tee -a $CONSTS_PATH
+done
+echo "  }" | tee -a $CONSTS_PATH
+echo "};" | tee -a $CONSTS_PATH
