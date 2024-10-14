0ec9b9f79a31d879f1cd15959ab9091f4eb1bca7
BayernMuller <cjjunk29@gmail.com>
2024-10-15 08:20:16 +0900

    feat: add section numbering to sidebar content in consts.ts

--- build.sh
+++ build.sh
@@ -32,17 +32,20 @@ done
 # add sidebar consts
 echo "export const SIDEBAR: Sidebar = {" | tee -a $CONSTS_PATH
 echo "  ko: {" | tee -a $CONSTS_PATH
+i=1
 for chapter in $chapters; do
     header=$(basename "${chapter/[0-9]_/}")
     header=${header//_/ }
     echo "    '$header': {" | tee -a $CONSTS_PATH
     
+    j=1
     sub_chapters=$(find "$chapter" -maxdepth 1 -type d -name '[0-9][0-9]_*' | sort -V)
     for sub_chapter in $sub_chapters; do
         sub_header=$(basename "${sub_chapter/[0-9][0-9]_/}")
         sub_header=${sub_header//_/ }
         echo "      '$sub_header': [" | tee -a $CONSTS_PATH
         
+        k=1
         contents=$(find "$sub_chapter" -maxdepth 1 -type f -name '*.mdx' | sort -V)
         for content in $contents; do
             content=${content/.mdx/}
@@ -52,18 +55,25 @@ for chapter in $chapters; do
             content_name=${content_name//[0-9][0-9]_/}
             content_name=${content_name//_/ }
 
+            # make index like: 1.2.2
+            index="$i.$j.$k"
+            content_name="$index. $content_name"
+
             # lowercase "content"
             content=$(echo "$content" | tr '[:upper:]' '[:lower:]')
 
             # remove ( and ) from content
             content=${content//\(/}
             content=${content//\)/}
-
+    
             echo "        { text: '$content_name', link: '/ko/$content' }," | tee -a $CONSTS_PATH
+            k=$((k+1))
         done
         echo "      ]," | tee -a $CONSTS_PATH
+        j=$((j+1))
     done
     echo "    }," | tee -a $CONSTS_PATH
+    i=$((i+1))
 done
 echo "  }" | tee -a $CONSTS_PATH
 echo "};" | tee -a $CONSTS_PATH
