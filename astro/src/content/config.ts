39b97d19ab2497aba6fc8088106ecd133cd25793
BayernMuller <cjjunk29@gmail.com>
2024-10-15 15:28:32 +0900

    fix: conditionally render title and update schema default

--- astro/src/content/config.ts
+++ astro/src/content/config.ts
@@ -3,7 +3,7 @@ import { SITE } from '../consts'
 
 const docs = defineCollection({
   schema: z.object({
-    title: z.string().default(SITE.title),
+    title: z.string().default(''),
     description: z.string().default(SITE.description),
     lang: z.literal('en-us').default(SITE.defaultLanguage),
     dir: z.union([z.literal('ltr'), z.literal('rtl')]).default('ltr'),
